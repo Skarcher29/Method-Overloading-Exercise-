c95fdf063d8a34a35ffac3fabc886565057ff28d
Skarcher29 <samanthakarcher@gmail.com>
2023-12-02 00:03:19 -0600

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace MethodOverloading
+﻿using System;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        { 
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var sum = a + b;
+            var ones = 1 | -1;
+
+            if (isTrue == true && sum == ones)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isTrue == true && sum > ones | sum < ones)
+            {
+                 return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+               
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var addInt = Add(1, 2);
+            var addDecimal = Add(0.69m, 3.51m);
+            var addString = Add(34, 35, true);
+
+            Console.WriteLine($"Add Integer answer: {addInt}. | Add Decimal answer: {addDecimal}. | Add String answer: {addString}.");
         }
     }
 }
